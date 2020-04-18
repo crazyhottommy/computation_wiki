b9e7bd99de71d62a8a0d44ed49cabf730c2d220e
Ming Tang <tangming2005@gmail.com>
2020-04-18 14:43:26 -0400

    add shell wrapper

---
+++ build_methods.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## strict check
+set -euo pipefail
+
+DOCDIR="$HOME"/Dropbox (Partners HealthCare)/github_repos/computation_wiki
+#### DANGER ####
+## MAKE SURE TO HAVE VALID PATH HERE AS SCRIPT WILL NOT CHECK FOR PATH
+## rsync may overwrite or worse, delete files on remote node.
+
+if [[ ! -d "$DOCDIR" || ! -x "$DOCDIR" ]]; then
+	echo -e "\nERROR: DOCDIR does not exists or not accesible at $DOCDIR\n" >&2
+	exit 1
+fi
+
+## build docs
+cd "$DOCDIR" && \
+mkdocs build --clean && echo -e "\nINFO: Built updated docs\n" && \
+mkdocs gh-deploy --clean -m "published using commit: {sha} and mkdocs {version}"
+
+## Build hook
+## curl -X POST -d {} https://api.netlify.com/build_hooks/***
+## END ##
---
+++ serve_test_local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## strict check
+set -euo pipefail
+
+DOCDIR="$HOME"/Dropbox (Partners HealthCare)/github_repos/computation_wiki
+
+if [[ ! -d "$DOCDIR" || ! -x "$DOCDIR" ]]; then
+	echo -e "\nERROR: DOCDIR does not exists or not accesible at $DOCDIR\n" >&2
+	exit 1
+fi
+
+## activate python3
+cd "$DOCDIR" && echo -e "\nWorkdir is $DOCDIR\n"
+mkdocs -q serve
+
+## END ##
